4c22369f68a630c54a2ff32eb8071e651e7878a7
Daniel Charvát <dan@localazy.com>
2024-08-26 13:38:35 +0200

    🔧 build: Drop json files from distribution.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 export type { Language } from "./language";
 export * from "./localazy-languages";
-import * as localizedLanguagesList from "./language-lists/localized-language-list.json";
-export { localizedLanguagesList };
 export * from "./locales"
-export * from "./language-lists/translated-language-lists";
