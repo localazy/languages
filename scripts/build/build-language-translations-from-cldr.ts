e0abda33ad1ff6d2e899f09caf3373244502b92f
Daniel Charvat <dan@localazy.com>
2025-03-31 11:07:06 +0200

    ✨ feat: Add german translations.

--- scripts/build/build-language-translations-from-cldr.ts
+++ scripts/build/build-language-translations-from-cldr.ts
@@ -1,6 +1,5 @@
-import fse from 'fs-extra';
-import cldrList from "../../src/language-lists/cldr-multilingual-language-list.json"
-
+import fse from "fs-extra";
+import cldrList from "../../src/language-lists/cldr-multilingual-language-list.json";
 
 export type ParsedLanguageLocale = {
   languageCode: string;
@@ -8,9 +7,13 @@ export type ParsedLanguageLocale = {
   script?: string;
 };
 
-const languages = ['cs', 'es'];
+const languages = ["cs", "es", "de"];
 
-const substringBefore = (value: string, before: string, missingDelimiterValue?: string): string => {
+const substringBefore = (
+  value: string,
+  before: string,
+  missingDelimiterValue?: string
+): string => {
   const beforeIndex = value.indexOf(before);
   const substringBefore = value.substring(0, beforeIndex);
 
@@ -21,7 +24,11 @@ const substringBefore = (value: string, before: string, missingDelimiterValue?: 
   return substringBefore;
 };
 
-const substringAfter = (value: string, after: string, missingDelimiterValue?: string): string => {
+const substringAfter = (
+  value: string,
+  after: string,
+  missingDelimiterValue?: string
+): string => {
   const afterIndex = value.indexOf(after);
   const afterLength = after.length;
   const substringAfter = value.substring(afterIndex + afterLength);
@@ -32,8 +39,6 @@ const substringAfter = (value: string, after: string, missingDelimiterValue?: st
 
   return substringAfter;
 };
-
-
 
 const parseLanguageLocale = (localeCode: string): ParsedLanguageLocale => {
   const languageCode = substringBefore(substringBefore(localeCode, "_"), "#");
@@ -48,24 +53,24 @@ const parseLanguageLocale = (localeCode: string): ParsedLanguageLocale => {
 };
 
 const firstUpperCaseData = (text: string) => {
-  return text.charAt(0).toUpperCase() + text.slice(1)
-}
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
 
 languages.forEach(language => {
   const englishFileName = `./src/language-lists/translations/en.json`;
   const translationFileName = `./src/language-lists/translations/${language}.json`;
   fse.ensureFileSync(translationFileName);
   fse.ensureFileSync(englishFileName);
-  const translationFile = fse.readFileSync(translationFileName, 'utf8');
+  const translationFile = fse.readFileSync(translationFileName, "utf8");
   const translationFileData = JSON.parse(translationFile || "{}");
 
-  const englishFile = fse.readFileSync(englishFileName, 'utf8');
+  const englishFile = fse.readFileSync(englishFileName, "utf8");
   const englishFileData = JSON.parse(englishFile || "{}");
 
-  Object.keys(englishFileData).forEach((locale) => {
-    const {languageCode, region, script} = parseLanguageLocale(locale);
+  Object.keys(englishFileData).forEach(locale => {
+    const { languageCode, region, script } = parseLanguageLocale(locale);
     const languageTranslations = cldrList.languages[languageCode];
-    let translation = '';
+    let translation = "";
     if (languageTranslations && languageTranslations[language]) {
       translation = firstUpperCaseData(languageTranslations[language]);
     }
@@ -90,12 +95,14 @@ languages.forEach(language => {
       }
     }
 
-    console.log(locale, translation)
-    console.log("____")
+    console.log(locale, translation);
+    console.log("____");
     translationFileData[locale] = translation;
   });
 
-
-
-  fse.writeFileSync(translationFileName, JSON.stringify(translationFileData, null, 2), 'utf8');
+  fse.writeFileSync(
+    translationFileName,
+    JSON.stringify(translationFileData, null, 2),
+    "utf8"
+  );
 });
